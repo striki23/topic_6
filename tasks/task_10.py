3620d63ae3bd509bf5c510f676e79aaa6a79b186
Dasha Demidova <striki06@mail.ru>
2023-11-07 13:51:55 +0300

    done

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -6,5 +6,27 @@ ALPHABETS = {
 }
 
 print("Добро пожаловать в программу \"Буква-Детектив\"!", end="\n\n")
+print('Выберите алфавит:\n 1. Латинский,\n 2. Кириллица\n')
 
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+# Уверена, что код можно оптимизировать...
+
+choice_alph = int(input('Введите номер алфавита: '))
+match choice_alph:
+    case 1:
+        choice_letter_l = input('Введите букву латинского алфавита: ').upper()
+        if choice_letter_l in ALPHABETS["en_vowels"]:
+            print(f'{choice_letter_l} - гласная буква!')
+        elif choice_letter_l in ALPHABETS["en_consonants"]:
+            print(f'{choice_letter_l} - согласная буква!')
+        else:
+            print('Упс! Неизвестная буква. Попробуйте другую!')
+    case 2:
+        choice_letter_k = input('Введите букву кириллицы: ').upper()
+        if choice_letter_k in ALPHABETS["ru_vowels"]:
+            print(f'{choice_letter_k} - гласная буква!')
+        elif choice_letter_k in ALPHABETS["ru_consonants"]:
+            print(f'{choice_letter_k} - согласная буква!')
+        else:
+            print('Упс! Неизвестная буква. Попробуйте другую!')
+    case _:
+        print('Упс! Выбран неверный режим. Попробуйте ещё раз...')
--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,1 +1,15 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+number = int(input())
+
+# 1. Решение с помощью тернарного оператора
+# result = 0 if number == 0 else 1 if number > 0 else -1
+
+# 2. Решение с помощью условных операторов
+if number == 0:
+    result = 0
+elif number > 0:
+    result = 1
+else:
+    result = -1
+
+print(result)
+
